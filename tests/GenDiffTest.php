70e01723ef545049392331dd4db0fd8411f15f88
Alex Karakin <karakin.alexandr@gmail.com>
2020-12-16 16:16:45 +0200

    edited

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -6,9 +6,9 @@ $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 
 if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
+    include_once $autoloadPath1;
 } else {
-    require_once $autoloadPath2;
+    include_once $autoloadPath2;
 }
 
 use PHPUnit\Framework\TestCase;
