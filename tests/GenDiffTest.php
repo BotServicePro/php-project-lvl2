0adb8f0e1b6200fd87903d5229d1793246b6a80e
Alex Karakin <karakin.alexandr@gmail.com>
2020-12-16 18:16:44 +0200

    edited

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -6,15 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 use function Differ\GenDiff\genDiff;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 class GenDiffTest extends TestCase
 {
     public function diffTest()
