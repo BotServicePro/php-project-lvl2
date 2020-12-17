42edc8e9d149c1b669c94c96753f576bcb747fe9
Alex Karakin <karakin.alexandr@gmail.com>
2020-12-17 16:14:06 +0200

    edited

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace Differ\Tests;
+namespace Differ\GenDiffTest;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    include_once $autoloadPath1;
+} else {
+    include_once $autoloadPath2;
+}
 
 use PHPUnit\Framework\TestCase;
 
@@ -10,15 +19,8 @@ class GenDiffTest extends TestCase
 {
     public function diffTest()
     {
-        $expected1 = "{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}
-";
+        $path1 = "fixtures/difftest.txt";
+        $expected1 = file_get_contents($path1);
         $this->assertEquals($expected1, genDiff('file1.json', 'file2.json'));
     }
 }
