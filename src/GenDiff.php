6faa32578220f2ee408da0b57165f55f90794b88
Alex Karakin <karakin.alexandr@gmail.com>
2020-12-30 17:01:20 +0200

    restructured functions

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -2,12 +2,31 @@
 
 namespace Differ\GenDiff;
 
+use Symfony\Component\Yaml\Yaml;
+
 use function Funct\Collection\flattenAll;
+use function Differ\Parsers\parser;
 
 const STARTTOSORTFROMSYMBOL = 4;
 
+function fileExtensionDataExtractor($fileName)
+{
+    $jsonExtension = (int) -4;
+    $ymlExtension = (int) -3;
+    if (substr($fileName, $ymlExtension) === 'yml') {
+        $file = file_get_contents($fileName);
+        $data = (array) Yaml::parse($file, Yaml::PARSE_OBJECT_FOR_MAP); // результат как объект
+    }
+    if (substr($fileName, $jsonExtension) === 'json') {
+        $file = file_get_contents($fileName);
+        $data = json_decode($file, $associative = true);
+    }
+    return $data;
+}
+
 function genDiff($path1, $path2)
 {
+    // делаем пути
     $path1 = $_SERVER['DOCUMENT_ROOT'] . $path1;
     $path2 = $_SERVER['DOCUMENT_ROOT'] . $path2;
 
@@ -15,38 +34,16 @@ function genDiff($path1, $path2)
         $path1 = '../' . $path1;
         $path2 = '../' . $path2;
     }
-    $readFirstFile = file_get_contents($path1);
-    $readSecondFile = file_get_contents($path2);
-    $firstFile = json_decode($readFirstFile, $associative = true); // тру означает возврат в виде массива
-    $secondFile = json_decode($readSecondFile, $associative = true);
-    $wasDeletedTemp = array_diff_key($firstFile, $secondFile); // то что было удалено из первого
-    foreach ($wasDeletedTemp as $key => $value) {
-        $wasDeleted[] = '  - ' . $key . ': ' . var_export($value, true); // var_export фактический результат
-    }
 
-    $wasNotChangedTemp = array_intersect_assoc($firstFile, $secondFile);
-    foreach ($wasNotChangedTemp as $key => $value) {
-        $wasNotChanged[] = '    ' . $key . ': ' . var_export($value, true);
-    }
-
-    $wasAddedTemp = array_diff_key($secondFile, $firstFile); // то что добавилось во второй массив
-    foreach ($wasAddedTemp as $key => $value) {
-        $wasAdded[] = '  + ' . $key . ': ' . var_export($value, true);
-    }
-
-    foreach ($firstFile as $key => $value) {
-        if (array_key_exists($key, $secondFile) === true && $firstFile[$key] !== $secondFile[$key]) {
-            $wasChanged[] = ['  - ' . $key .  ': ' . $firstFile[$key],
-                '  + ' . $key . ': ' . $secondFile[$key]];
-        }
-    }
-
-    $mergedResults = array_merge($wasDeleted, $wasNotChanged, $wasAdded, $wasChanged);
-    $sortedArr = flattenAll($mergedResults);
+    $firstFile = fileExtensionDataExtractor($path1);
+    $secondFile = fileExtensionDataExtractor($path2);
+    $parsedData = parser($firstFile, $secondFile);
+    $sortedArr = flattenAll($parsedData);
     usort($sortedArr, function ($a, $b) {
         return substr($a, STARTTOSORTFROMSYMBOL, 1) <=> substr($b, STARTTOSORTFROMSYMBOL, 1);
     });
 
+    // сборка в финальный вид
     $stringWithSemicolons = "{" . "\n" . implode("\n", $sortedArr) . "\n" . "}";
     $finalResult = str_replace("'", '', $stringWithSemicolons);
     print_r($finalResult);
