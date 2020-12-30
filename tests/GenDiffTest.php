a5c3c2d0c20dcbc53b6307322cdacc3d07e82981
Alex Karakin <karakin.alexandr@gmail.com>
2020-12-30 17:01:33 +0200

    added few tests

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -10,8 +10,24 @@ class GenDiffTest extends TestCase
 {
     public function testGenDiff()
     {
-        $source = __DIR__ . "/fixtures/difftest.txt";
+        // Сравнение двух json
+        $source = __DIR__ . "/fixtures/compareResult.txt";
         $expected = file_get_contents($source);
         $this->assertEquals($expected, genDiff('file1.json', 'file2.json'));
+
+        // Сравнение двух yml
+        $source = __DIR__ . "/fixtures/compareResult.txt";
+        $expected2 = file_get_contents($source);
+        $this->assertEquals($expected2, genDiff('file1.yml', 'file2.yml'));
+
+        // Сравнение yml и json
+        $source = __DIR__ . "/fixtures/compareResult.txt";
+        $expected2 = file_get_contents($source);
+        $this->assertEquals($expected2, genDiff('file1.yml', 'file2.json'));
+
+        // Сравнение json и yml
+        $source = __DIR__ . "/fixtures/compareResult.txt";
+        $expected2 = file_get_contents($source);
+        $this->assertEquals($expected2, genDiff('file1.json', 'file2.yml'));
     }
 }
