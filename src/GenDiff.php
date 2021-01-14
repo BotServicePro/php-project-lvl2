7ed4908b5357ebd4083982c5d59cdcdf36578f2e
akarakin <bulik889@gmail.com>
2021-01-14 22:00:23 +0200

    edited

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -18,7 +18,7 @@ function genDiff($path1, $path2) // главная функция
     $firstFile = parser($path1); // получаем данные из файла в том виде в каком они есть
     $secondFile = parser($path2); // получаем данные из файла в том виде в каком они есть
     $differedData = diffData($firstFile, $secondFile); // получаем различия файлов и плоских и жирных
-    $finalResult = builder($differedData);
+    $finalResult = formatter($differedData);
 
     return $finalResult;
 }
@@ -28,6 +28,7 @@ function diffData($firstFile, $secondFile)
     $firstFile = (array) $firstFile;
     $secondFile = (array) $secondFile;
     $uniqueKeys = array_keys(array_merge($firstFile, $secondFile));
+    // через замыкание use
     $data = array_map(function ($key) use ($firstFile, $secondFile) {
         // если текущий ключ не существует в первом файле, но присутствует во втором
         if (!array_key_exists($key, $firstFile)) {
@@ -36,20 +37,24 @@ function diffData($firstFile, $secondFile)
         } elseif (!array_key_exists($key, $secondFile)) {
             return ['key' => $key, 'value' => $firstFile[$key], 'status' => 'removed'];
         }
-        $nodeFirst = $firstFile[$key]; // вложка
-        $nodeSecond = $secondFile[$key]; // вложка
 
-        // если объект то это вложенные данные
+        $nodeFirst = $firstFile[$key];
+        $nodeSecond = $secondFile[$key];
+
+        print_r($nodeFirst);
+        print_r($nodeSecond);
+
         if (is_object($nodeFirst) && is_object($nodeSecond)) {
             $children = diffData($nodeFirst, $nodeSecond);
-            return ['key' => $key, 'type' => 'parent', 'children' => $children];
+            return ['key' => $key, 'type' => 'nested', 'children' => $children];
         }
         if ($nodeFirst === $nodeSecond) {
-            return  ['key' => $key, 'value' => $nodeFirst, 'status' => 'nested'];
+            return  ['key' => $key, 'value' => $nodeFirst, 'status' => 'not changed'];
         } else {
             return ['key' => $key, 'newValue' => $nodeSecond, 'oldValue' => $nodeFirst, 'status' => 'changed'];
         }
     }, $uniqueKeys);
+    print_r($data);
     return $data;
 }
 
@@ -58,7 +63,7 @@ function isMultidimensional($data)
     // функция должна вернуть true или false если объект/массивы многомерные
     foreach ($data as $item) {
         foreach ($item as $subItem) {
-            if (is_array($subItem) === true) {
+            if (is_array($subItem) === true || is_object($subItem) === true) {
                 // если хотя бы один элемент в массиве будет объектом или массивом - это многомерные данные
                 return true;
             }
@@ -67,17 +72,17 @@ function isMultidimensional($data)
     return false;
 }
 
-function builder($data)
+function formatter($data)
 {
-    $data = json_decode(json_encode($data), true);
-    print_r($data);
+    //$data = json_decode(json_encode($data), true);
+    //print_r($data);
     if (isMultidimensional($data) === false) { // если одномерные данные
         foreach ($data as $item) {
             if ($item['status'] === 'changed') {
                 $tempResult[] = "  - " . $item['key'] . ': ' . var_export($item['oldValue'], true);
                 $tempResult[] = "  + " . $item['key'] . ': ' . var_export($item['newValue'], true);
             }
-            if ($item['status'] === 'nested') {
+            if ($item['status'] === 'not changed') {
                 $tempResult[] = "    " . $item['key'] . ': ' . var_export($item['value'], true);
             }
             if ($item['status'] === 'added') {
@@ -88,15 +93,30 @@ function builder($data)
             }
         }
 
-        print_r($tempResult);
+        //print_r($tempResult);
         usort($tempResult, function ($a, $b) {
             return substr($a, STARTTOSORTFROMSYMBOL, 1) <=> substr($b, STARTTOSORTFROMSYMBOL, 1);
         });
         $tempResult = str_replace("'", '', $tempResult);
-        print_r($tempResult);
+        //print_r($tempResult);
         $finalResult = "{" . "\n" . implode("\n", $tempResult) . "\n" . "}";
-        print_r($finalResult);
-    } elseif (isMultidimensional($data) === true) {
+        //print_r($finalResult);
+    } elseif (isMultidimensional($data) === true) { // если данные многомерные
+        foreach ($data as $item) {
+            //print_r($item);
+            if ($item['status'] === 'changed') {
+            }
+            if ($item['status'] === 'not changed') {
+                $tempResult[] = "    " . $item['key'] . ': ' . var_export($item['value'], true);
+            }
+            if ($item['status'] === 'added') {
+                $tempResult[] = "  + " . $item['key'] . ': ' . var_export($item['value'], true);
+            }
+            if ($item['status'] === 'removed') {
+                $tempResult[] = "  - " . $item['key'] . ': ' . var_export($item['value'], true);
+            }
+        }
     }
+    //print_r($tempResult);
     return $finalResult;
 }
