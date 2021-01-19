a2e8a1ddb603517573444b80ab8be9f11ef63881
akarakin <bulik889@gmail.com>
2021-01-19 22:37:17 +0200

    tests updated

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -13,22 +13,22 @@ class GenDiffTest extends TestCase
         // Сравнение двух простых json
         $source = __DIR__ . "/fixtures/compareResult.txt";
         $expected = file_get_contents($source);
-        $this->assertEquals($expected, genDiff('file1.json', 'file2.json'));
+        $this->assertEquals($expected, genDiff('file1.json', 'file2.json', $format = 'stylish'));
 
         // Сравнение двух простых yml
         $source = __DIR__ . "/fixtures/compareResult.txt";
         $expected2 = file_get_contents($source);
-        $this->assertEquals($expected2, genDiff('file1.yml', 'file2.yml'));
+        $this->assertEquals($expected2, genDiff('file1.yml', 'file2.yml', $format = 'stylish'));
 
         // Сравнение простых yml и json
         $source = __DIR__ . "/fixtures/compareResult.txt";
         $expected2 = file_get_contents($source);
-        $this->assertEquals($expected2, genDiff('file1.yml', 'file2.json'));
+        $this->assertEquals($expected2, genDiff('file1.yml', 'file2.json', $format = 'stylish'));
 
         // Сравнение простых json и yml
         $source = __DIR__ . "/fixtures/compareResult.txt";
         $expected2 = file_get_contents($source);
-        $this->assertEquals($expected2, genDiff('file1.json', 'file2.yml'));
+        $this->assertEquals($expected2, genDiff('file1.json', 'file2.yml', $format = 'stylish'));
 
 //        // Сравнение рекурсивных json и json
 //        $source = __DIR__ . "/fixtures/compareRecursiveResult.txt";
