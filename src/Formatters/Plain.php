88ec5760360e068cf67a1ce5259145fb01094e4e
akarakin <bulik889@gmail.com>
2021-01-26 13:40:44 +0200

    not ready yet

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -8,5 +8,26 @@ const STARTTOSORTFROMSYMBOL = 4;
 
 function plain($data)
 {
-    return $data;
+
+    // если содержимое НЕ ОБъЕКТ И НЕ МАССИВ, возвращаем ключ
+    if (is_array($data) === false && is_object($data) === false) {
+        return [$data['key']];
+    }
+
+
+    // попробуем собрать полный путь до файлов
+    $onlyPath = array_reduce($data, function ($acc, $item) {
+        //print_r($item);
+
+        if ($item['type'] === 'nested') {
+            return $acc[$item['key']];
+        }
+
+    }, []);
+    print_r($onlyPath);
+
+
+
+
+
 }
