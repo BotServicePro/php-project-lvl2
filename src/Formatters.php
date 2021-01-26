7d7519519b23d7008d6af9ab2361ca7634d82cb3
akarakin <bulik889@gmail.com>
2021-01-26 13:38:38 +0200

    deleted unused function

--- src/Formatters.php
+++ src/Formatters.php
@@ -2,13 +2,13 @@
 
 namespace Differ\Formatters;
 
-use function Differ\Formatters\Stylish\stylish;
+use function Differ\Formatters\Stylish\render;
 use function Differ\Formatters\Plain\plain;
 
 function formateIt($data, $type)
 {
     if ($type === 'stylish') {
-        return stylish($data);
+        return render($data);
     } elseif ($type === 'plain') {
         return plain($data);
     }
