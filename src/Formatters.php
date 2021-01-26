f3546bba98efbbbd34da9b848a44bc80d5fe3883
akarakin <bulik889@gmail.com>
2021-01-26 15:57:35 +0200

    added json format

--- src/Formatters.php
+++ src/Formatters.php
@@ -4,6 +4,7 @@ namespace Differ\Formatters;
 
 use function Differ\Formatters\Stylish\render as stylish;
 use function Differ\Formatters\Plain\render as plain;
+use function Differ\Formatters\Json\render as json;
 
 function formateIt($data, $type)
 {
@@ -11,5 +12,7 @@ function formateIt($data, $type)
         return stylish($data);
     } elseif ($type === 'plain') {
         return plain($data);
+    } elseif ($type === 'json') {
+        return json($data);
     }
 }
