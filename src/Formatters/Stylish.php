a26b427915dbee3da93529f37d3aa78a080a6ae6
akarakin <bulik889@gmail.com>
2021-01-31 21:53:24 +0200

    comments cleared

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -31,18 +31,26 @@ function stylish($data, $depth)
                 $stringedData = convertToString($item['value'], $depth);
                 return $tabulation . $minus . $item['key'] . $doublePoint . $stringedData;
             case 'changed':
+                //конвертим старое значение
                 $oldValue = convertToString($item['oldValue'], $depth);
+                //конвертим новоe значние
                 $newValue = convertToString($item['newValue'], $depth);
+                // записываем в переменне с форматирование
                 $stringedNewValue = $tabulation . $plus . $item['key'] . $doublePoint . $newValue;
                 $stringedOldValue = $tabulation . $minus . $item['key'] . $doublePoint . $oldValue;
+                // возвращаем две новые строки
                 return $stringedOldValue . "\n" . $stringedNewValue;
             case 'unchanged':
                 $stringedData = convertToString($item['value'], $depth);
                 return $tabulation . $space . $item['key'] . $doublePoint . $stringedData;
             case 'nested':
+                // получаем то что вложено
                 $children = $item['children'];
+                // делаем первую строку с ключом и отступом
                 $stringedHeader = $tabulation . $space . $item['key'] . $doublePoint .  '{';
+                // тело данных, вызываем функцию рекурсивно с вложенными данными
                 $body = stylish($children, $depth + 1);
+                // пересобираем тело с новой строки
                 $stringedBody = implode("\n", $body);
                 return $stringedHeader . "\n" . $stringedBody . "\n" . $tabulation . $space . '}';
         }
