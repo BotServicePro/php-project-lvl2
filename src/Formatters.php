ef5fd5555cac06a49cae46ecf46f5cae59227217
akarakin <bulik889@gmail.com>
2021-01-31 21:55:06 +0200

    function renamed

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,7 +6,7 @@ use function Differ\Formatters\Stylish\render as stylish;
 use function Differ\Formatters\Plain\render as plain;
 use function Differ\Formatters\Json\render as json;
 
-function formateIt($data, $type)
+function astToStringConverter($data, $type)
 {
     if ($type === 'stylish') {
         return stylish($data);
