7adffebfe64db35a9503c5317e1054e7fdc8beb4
akarakin <bulik889@gmail.com>
2021-02-02 13:26:59 +0200

    added check for yaml extension

--- src/FileExtractor.php
+++ src/FileExtractor.php
@@ -6,10 +6,15 @@ use Symfony\Component\Yaml\Yaml;
 
 const JSONEXTENSION = -4;
 const YMLEXTENSION = -3;
+const YAMLEXTENSION = -4;
 
 function extractData($fileName)
 {
     if (substr($fileName, YMLEXTENSION) === 'yml') { // если это yml файлы
+        $file = file_get_contents($fileName);
+        $data = Yaml::parse($file, Yaml::PARSE_OBJECT_FOR_MAP);
+    }
+    if (substr($fileName, YAMLEXTENSION) === 'yaml') { // если это yml файлы
         $file = file_get_contents($fileName);
         $data = Yaml::parse($file, Yaml::PARSE_OBJECT_FOR_MAP);
     }
