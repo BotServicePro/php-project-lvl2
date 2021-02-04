4a7f5a5056cef4acedb4a9a9d81cdc2fa55cea4a
akarakin <bulik889@gmail.com>
2021-02-04 13:35:09 +0200

    use function renamed for parsers

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -4,7 +4,7 @@ namespace Differ\GenDiff;
 
 use Exception;
 
-use function Differ\FileExtractor\extractData;
+use function Differ\Parsers\extractData;
 use function Differ\Formatters\astToStringConverter;
 
 function makeFilePath($path)
