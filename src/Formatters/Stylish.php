0985821895c96564c73f53e8a8e9d892d9da6885
akarakin <bulik889@gmail.com>
2021-02-09 11:46:47 +0200

    functon renamed and changed (array) to get_object_vars

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -19,19 +19,19 @@ function stylish($data, $depth)
         $tabulation = str_repeat('    ', $depth - 1);
         switch ($item['type']) {
             case 'added':
-                $stringedData = stringedData($item['value'], $depth);
+                $stringedData = strigify($item['value'], $depth);
                 return "$tabulation  + {$item['key']}: $stringedData";
             case 'removed':
-                $stringedData = stringedData($item['value'], $depth);
+                $stringedData = strigify($item['value'], $depth);
                 return "$tabulation  - {$item['key']}: $stringedData";
             case 'changed':
-                $oldValue = stringedData($item['oldValue'], $depth);
-                $newValue = stringedData($item['newValue'], $depth);
+                $oldValue = strigify($item['oldValue'], $depth);
+                $newValue = strigify($item['newValue'], $depth);
                 $stringedNewValue = "$tabulation  + {$item['key']}: $newValue";
                 $stringedOldValue = "$tabulation  - {$item['key']}: $oldValue";
                 return $stringedOldValue . "\n" . $stringedNewValue;
             case 'unchanged':
-                $stringedData = stringedData($item['value'], $depth);
+                $stringedData = strigify($item['value'], $depth);
                 return "$tabulation    {$item['key']}: $stringedData";
             case 'nested':
                 $children = $item['children'];
@@ -44,10 +44,10 @@ function stylish($data, $depth)
     return $result;
 }
 
-function stringedData($data, $depth)
+function strigify($data, $depth)
 {
     if (is_object($data)) {
-        $data = (array) $data;
+        $data = get_object_vars($data);
     }
     if ($data === null || is_bool($data)) {
         return strtolower(var_export($data, true));
@@ -64,18 +64,18 @@ function stringedData($data, $depth)
     $stringedData = array_map(function ($key, $value) use ($depth, $space, $tabulation, $string) {
         if (is_object($value)) {
             $value = (array) $value;
-            $stringedValue = stringedData($value, $depth + 1);
+            $stringedValue = strigify($value, $depth + 1);
             return "{$tabulation}{$space}{$key}: $stringedValue";
         }
         if (!is_array($value) && !is_object($value)) {
             return "{$tabulation}{$space}{$key}: $value";
         }
         if (is_array($value) && is_array($value[key($value)])) {
-            $stringed = stringedData($value, $depth + 1);
+            $stringed = strigify($value, $depth + 1);
             return "{$tabulation}{$space}{$key}: $stringed";
         }
         if (is_array($value) && !is_array($value[key($value)])) {
-            $stringed2 = stringedData($value, $depth + 1);
+            $stringed2 = strigify($value, $depth + 1);
             return "{$string}\n{$tabulation}{$space}{$key}: $stringed2";
         }
     }, array_keys($data), $data);
