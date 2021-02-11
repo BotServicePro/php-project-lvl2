44d7046d6b66539d53d5d89144805a717651d1e5
akarakin <bulik889@gmail.com>
2021-02-11 09:55:52 +0200

    Removed print_r

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,13 +6,10 @@ function format($tree, $format)
 {
     switch ($format) {
         case 'stylish':
-            print_r(Stylish\render($tree));
             return Stylish\render($tree);
         case 'plain':
-            print_r(Plain\render($tree));
             return Plain\render($tree);
         case 'json':
-            print_r(Json\render($tree));
             return Json\render($tree);
         default:
             throw new \Exception("Error, wrong format!");
