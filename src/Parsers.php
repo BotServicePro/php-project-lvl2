dcfd525e711f020c95faae576b01c35092536a2f
akarakin <bulik889@gmail.com>
2021-02-15 18:32:43 +0200

    Variable renamed

--- src/Parsers.php
+++ src/Parsers.php
@@ -4,14 +4,14 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parse($fileData, $extension): object
+function parse($data, $extension): object
 {
     switch ($extension) {
         case 'yaml':
         case 'yml':
-            return Yaml::parse($fileData, Yaml::PARSE_OBJECT_FOR_MAP);
+            return Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
         case 'json':
-            return json_decode($fileData);
+            return json_decode($data);
         default:
             throw new \Exception("Unknown file extension $extension!");
     }
