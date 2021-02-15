eaa4550736e66693698dd8166ea166c30c81ec6d
akarakin <bulik889@gmail.com>
2021-02-15 18:39:38 +0200

    Added new function

--- src/Differ.php
+++ src/Differ.php
@@ -9,20 +9,24 @@ use function Differ\Formatters\format;
 use function Funct\Collection\sortBy;
 use function Funct\Collection\union;
 
-function readFile($path): array
+function readFile($path): string
 {
     if (!file_exists($path)) {
         throw new Exception("File not found. Wrong filepath is: $path");
     }
-    return ['fileData' => file_get_contents($path), 'extension' => pathinfo($path, PATHINFO_EXTENSION)];
+    return file_get_contents($path);
+}
+
+function getExtension($path) {
+    return pathinfo($path, PATHINFO_EXTENSION);
 }
 
 function genDiff($path1, $path2, $format = 'stylish'): string
 {
     $firstPath = readFile($path1);
     $secondPath = readFile($path2);
-    $firstData = parse($firstPath['fileData'], $firstPath['extension']);
-    $secondData = parse($secondPath['fileData'], $secondPath['extension']);
+    $firstData = parse($firstPath, getExtension($path1));
+    $secondData = parse($secondPath, getExtension($path2));
     $differedTree = buildTree($firstData, $secondData);
     return format($differedTree, $format);
 }
