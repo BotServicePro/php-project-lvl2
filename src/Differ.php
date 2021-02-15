b440c764e91cf216db6aef5432b550726efe06ae
akarakin <bulik889@gmail.com>
2021-02-15 18:46:25 +0200

    Moved out function treebuilder

--- src/Differ.php
+++ src/Differ.php
@@ -6,8 +6,7 @@ use Exception;
 
 use function Differ\Parsers\parse;
 use function Differ\Formatters\format;
-use function Funct\Collection\sortBy;
-use function Funct\Collection\union;
+use function Differ\TreeBuilder\buildTree;
 
 function readFile($path): string
 {
@@ -32,29 +31,3 @@ function genDiff($path1, $path2, $format = 'stylish'): string
     return format($differedTree, $format);
 }
 
-function buildTree(object $firstData, object $secondData): array
-{
-    $uniqueKeys = union(array_keys(get_object_vars($firstData)), array_keys(get_object_vars($secondData)));
-    $sortedUniqueKeys = array_values(sortBy($uniqueKeys, fn ($key) => $key));
-    $data = array_map(function (string $key) use ($firstData, $secondData): array {
-        if (!property_exists($secondData, $key)) {
-            return ['key' => $key, 'value' => $firstData->$key, 'type' => 'removed'];
-        }
-        if (!property_exists($firstData, $key)) {
-            return ['key' => $key, 'value' => $secondData->$key, 'type' => 'added'];
-        }
-        if (is_object($firstData->$key) && is_object($secondData->$key)) {
-            return ['key' => $key, 'type' => 'nested', 'children' => buildTree($firstData->$key, $secondData->$key)];
-        }
-        if ($firstData->$key !== $secondData->$key) {
-            return [
-                'key' => $key,
-                'oldValue' => $firstData->$key,
-                'newValue' => $secondData->$key,
-                'type' => 'changed'
-            ];
-        }
-        return  ['key' => $key, 'value' => $firstData->$key, 'type' => 'unchanged'];
-    }, $sortedUniqueKeys);
-    return $data;
-}
